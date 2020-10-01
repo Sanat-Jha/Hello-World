7dc49b12026db3c40479583df331c60309f1a7be
Sanat-Jha <69027091+Sanat-Jha@users.noreply.github.com>
2020-10-01 11:06:04 +0530

    Added important things in python code

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,7 @@
+'''
+Python is an interpreted, high-level and general-purpose programming language. 
+Created by Guido van Rossum and first released in 1991, 
+Python's design philosophy emphasizes code readability with its notable use of significant whitespace. 
+'''
+#lets print hello world.
 print('Hello World')
